5cf0bfa11ed7781931e868a7dfb486235618351c
JJAV88 <avj88.jj@gmail.com>
2022-09-22 10:41:14 -0500

    new-prueba

--- agentxia/src/Components/Views/LandingPage/Contactanos.jsx
+++ agentxia/src/Components/Views/LandingPage/Contactanos.jsx
@@ -6,7 +6,7 @@ const Contactanos = () => {
     <div>
           <Form>
       <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
+        <Form.Label>Nombre</Form.Label>
         <Form.Control type="email" placeholder="Enter email" />
         <Form.Text className="text-muted">
           We'll never share your email with anyone else.
